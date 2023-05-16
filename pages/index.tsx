ac6ffb6e83a7affec16eabe95927bc29a3ca62f3
Yaman <yamix2000@gmail.com>
2023-05-16 13:58:45 +0200

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,60 @@
-import Image from 'next/image'
-import { SyntheticEvent, useEffect, useState } from 'react'
+import Image from "next/image";
+import { SyntheticEvent, useEffect, useState } from "react";
+
+type PredictionType = {
+  material: string;
+  bucket: string;
+  input_file: string;
+  output_file: string;
+};
 
 export default function Home() {
-  const [images, setImages] = useState<File[]>([])
-  const [imageUrls, setImageUrls] = useState<string[]>([])
-  const [results, setResults] = useState([])
+  const [images, setImages] = useState<File[]>([]);
+  const [imageUrls, setImageUrls] = useState<string[]>([]);
+  const [results, setResults] = useState([]);
 
   const handleImageChange = (event: SyntheticEvent) => {
-    setImages([...(event.target as HTMLInputElement).files])
-  }
+    setImages([...(event.target as HTMLInputElement).files]);
+  };
 
   const handleOnSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
     const form = e.target as HTMLFormElement;
-    const formData = new FormData(form)
-    if (images.length < 1) return
-    images.forEach(img => {
-      formData.append("file", img, img.name)
-    })
+    const formData = new FormData(form);
+    if (images.length < 1) return;
+    images.forEach((img) => {
+      formData.append("file", img, img.name);
+    });
 
     const res = await fetch(form.action, {
       method: form.method,
-      body: formData
-    })
-    const data = await res.json()
-    console.log(window.btoa(data[0]["detected_image"]))
+      body: formData,
+    });
+    const data: PredictionType = await res.json();
+    console.log(data);
 
-    setResults(data)
-  }
+    setResults(data);
+  };
 
   useEffect(() => {
-    if (images.length < 1) return
+    if (images.length < 1) return;
     const newImageUrls: string[] = [];
     images.forEach((img) => {
-      newImageUrls.push(
-        URL.createObjectURL(img)
-      )
-    })
+      newImageUrls.push(URL.createObjectURL(img));
+    });
     setImageUrls(newImageUrls);
-  }, [images])
-
-
+  }, [images]);
 
   return (
-    <div className='max-w-8xl p-5 mx-auto'>
-      <h1 className='text-7xl mt-20 font-bold'>kandidat-ui</h1>
+    <div className="max-w-8xl p-5 mx-auto">
+      <h1 className="text-7xl mt-20 font-bold">kandidat-ui</h1>
 
-      <form action="http://localhost:5000/predict" method='POST' onSubmit={handleOnSubmit}>
-        <div className='pt-10'>
+      <form
+        action="http://localhost:5000/predict"
+        method="POST"
+        onSubmit={handleOnSubmit}
+      >
+        <div className="pt-10">
           <input
             className="file:mr-4 file:py-2 file:px-4
               file:rounded-full file:border-0
@@ -56,34 +63,95 @@ export default function Home() {
               hover:file:bg-teal-100"
             type="file"
             multiple
-            accept='image/*'
+            accept="image/*"
             onChange={handleImageChange}
           />
         </div>
 
-        <div className='flex gap-6 my-10 px-10 bg-stone-900 rounded-md'>
+        <div className="flex gap-6 my-10 px-10 bg-stone-900 rounded-md">
           {imageUrls.map((imgUrl, index) => {
             return (
-              <div key={`${imgUrl}-${index}`} className='my-10'>
+              <div key={`${imgUrl}-${index}`} className="my-10">
                 <Image src={imgUrl} alt="image" width={200} height={200} />
               </div>
-            )
+            );
           })}
         </div>
-        <button className='py-2 px-4 rounded-lg border transition-all duration-150 bg-teal-50 text-teal-700 hover:bg-teal-100' type='submit'>Upload</button>
+        <button
+          className="py-2 px-4 rounded-lg border transition-all duration-150 bg-teal-50 text-teal-700 hover:bg-teal-100"
+          type="submit"
+        >
+          Upload
+        </button>
       </form>
 
-      <h2 className='text-7xl font-bold mt-5'>results</h2>
+      <h2 className="text-7xl font-bold mt-5">results</h2>
 
-      <div className='flex gap-6 my-10 px-10 bg-stone-900 rounded-md'>
+      <div className="flex gap-6 my-10 px-10 bg-stone-900 rounded-md">
         {results.map((result, index) => {
           return (
-            <div key={`${result}-${index}`} className='m-5'>
-              <Image src={`http://localhost:9000/${result.bucket}/${result.output_file}`} alt="image" width={400} height={400} />
+            <div key={`${result}-${index}`} className="m-5">
+              <h2 className="text-xl mt-4">
+                Material:{" "}
+                <span className="text-sm border px-5 py-1 rounded-full">
+                  {result.materials}
+                </span>
+              </h2>
+
+              <h2 className="text-xl mt-4">
+                Dimensions:{" "}
+                <span className="text-sm border px-5 py-1 rounded-full">
+                  Width: {result.dimensions.predicted_width} cm
+                </span>
+                <span className="text-sm border px-5 py-1 rounded-full ml-2">
+                  Height: {result.dimensions.predicted_height} cm
+                </span>
+              </h2>
+
+              <h2 className="text-xl mt-4 flex">
+                Colors:{" "}
+                {result.colors.map((color, index) => (
+                  <span
+                    key={`${color}-${index}`}
+                    style={{ backgroundColor: color }}
+                    className={`block w-8 h-8 rounded ml-3`}
+                  ></span>
+                ))}
+              </h2>
+
+              <div className="bg-stone-800 p-4 rounded mt-10">
+                <p className="p-0">Prediction:</p>
+                <Image
+                  src={`http://localhost:9000/${result.defects.bucket}/${result.defects.output_file}`}
+                  alt="image"
+                  width={400}
+                  height={400}
+                />
+              </div>
+
+              <div className="bg-stone-800 p-2 mt-4">
+                <p className="p-0">Dimension:</p>
+                <Image
+                  src={`http://localhost:9000/${result.dimensions.bucket}/${result.dimensions.output_file}`}
+                  alt="image"
+                  width={400}
+                  height={400}
+                />
+              </div>
+
+              <div className="bg-stone-800 p-2 mt-4">
+                <p className="p-0">Transparent:</p>
+                <Image
+                  src={`http://localhost:9000/${result.transparent.bucket}/${result.transparent.output_file}`}
+                  alt="image"
+                  width={400}
+                  height={400}
+                />
+              </div>
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
